278f2f6482f495ff7b6e4a4b8b4e38145473bb8f
Seonggyu-Han <damian20@naver.com>
2025-09-05 19:09:04 +0900

    Feat: 소셜 로그인 / 일반 회원가입 / 일반 로그인 구현 #89

---
+++ Seong9/scentlab/uauth/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+class UauthConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "uauth"
+
+    def ready(self):
+        import uauth.signals
