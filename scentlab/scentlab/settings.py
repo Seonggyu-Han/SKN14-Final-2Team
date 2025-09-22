6609fee88d0c8a8fa8d656e0e419a0aff45a38e7
Seonggyu-Han <damian20@naver.com>
2025-09-22 20:34:53 +0900

    Feat: chat.html, views.py multimodal FE #194

--- scentlab/scentlab/settings.py
+++ scentlab/scentlab/settings.py
@@ -208,8 +208,10 @@ ALLOWED_HOSTS.extend([
 ])
 
 # S3 storage settings (used by uauth.utils upload)
-AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME', '')
-AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', '')
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_REGION = os.getenv("AWS_REGION", "ap-northeast-2")
+AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
 
 # Media
 MEDIA_URL = '/media/'
